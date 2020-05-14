d0f50c6dc7ee0ee0572953042bf1bdc11dc2c499
Thiago Silva <thiago.silva@outlook.com>
2020-05-14 08:19:30 -0300

    Iniciando o projeto

---
+++ PaymentContracts/Entities/Contract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContracts.Entities
+{
+    class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public Installment Installment { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue, Installment installment)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            Installment = installment;
+        }
+    }
+}
---
+++ PaymentContracts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaymentContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
